691ec5c0ad937ecea25fb55c15b156640bc84163
christian phan <christian.k.phan@gmail.com>
2016-06-10 19:20:59 -0700

    edit

---
+++ problem3/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main()
+{
+
+	int num = 600851475143;
+	int prime = 1;
+	int largest =  0;
+
+	while(prime <= num)
+	{
+	if(num%prime == 0)
+	{
+		
+		while(prime = 2000)
+
+	}
+	prime++;
+
+	}
+
+
+	cout << largest;
+}
